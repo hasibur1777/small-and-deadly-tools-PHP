37fe96d4cecb01f933bebc019ca724baa6b84a10
Hasibur Rahman <110320388+hasibur1777@users.noreply.github.com>
2022-09-15 09:08:00 +0600

    Update Time Date Difference.php

--- Time Date Difference.php
+++ Time Date Difference.php
@@ -20,3 +20,8 @@ $date1 = date_create("2012-01-03 18:13:00");
 $date2 = date_create("2015-01-03 1:13:00");
 $diff = date_diff($date1, $date2);
 echo $diff->format("%y-%m-%d %h:%m:%s ");
+
+// add time date with time date
+$Date1 = '2022-09-15 09:00:00';
+$Date2 = date('Y-m-d H:m:s', strtotime($Date1 . " + 1 day 5 hours"));
+echo $Date2;
